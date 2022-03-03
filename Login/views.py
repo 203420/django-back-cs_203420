e9e678a5b32b37e42b454b43ae1205c75dec631e
203420 <203420@ids.upchiapas.edu.mx>
2022-03-03 09:58:18 -0600

    User profile creation

--- Login/views.py
+++ Login/views.py
@@ -23,7 +23,10 @@ class LoginAuth (ObtainAuthToken):
         return Response({
             'token': token.key,
             'user_id': user.pk,
+            'username': user.username,
             'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
         })
 
 
--- primerApp/settings.py
+++ primerApp/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.IsAuthenticated',),
-    'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework_simplejwt.authentication.JWTAuthentication',),
+    'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework.authentication.TokenAuthentication', 'rest_framework_simplejwt.authentication.JWTAuthentication',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
     
